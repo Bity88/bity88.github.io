3d1b299d2fcb8c2ed9351035367afaf5fde90992
Sandeep Baniya <baniyasandy32@gmail.com>
2019-05-31 22:36:16 -0500

    tipcalculator

---
+++ lab5/buggy4.js
@@ -0,0 +1,14 @@
+
+function somethingElse() {
+    if (5 > 2) {
+        alert("Bigger");
+    if (5 < 2) {
+        alert("Smaller");
+    }
+}
+
+}
+
+function gogogo() {
+    alert("Yay, it works!");
+}
