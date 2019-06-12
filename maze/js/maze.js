3c6f4049ded79f4daee83878e9ce32e3b3cddae7
Sandeep Baniya <baniyasandy32@gmail.com>
2019-06-11 23:43:24 -0500

    hello

--- maze/js/maze.js
+++ maze/js/maze.js
@@ -35,6 +35,24 @@ $(document).ready(function(){
 
             });
 
+            $("#maze").mouseleave(function(){
+
+                if(start==1){
+
+                    $("#maze div.boundary").addClass("youlose");
+                    start =0;
+                   
+                    }
+    
+                    $("#start").click(function(){
+    
+                        $("#maze div.boundary").removeClass("youlose");
+    
+    
+                    });
+
+            });
+
             $("#end").click(function(){
 
                 if(start==1){
